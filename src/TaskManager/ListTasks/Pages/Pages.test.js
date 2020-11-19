06eb3501beee68d9816ce0f715e8720cfe0a2f45
Tales Ferreira <talesmateus1999@hotmail.com>
2020-11-19 08:18:30 -0400

    Created tests for Pages module

--- src/TaskManager/ListTasks/Pages/Pages.test.js
+++ src/TaskManager/ListTasks/Pages/Pages.test.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import ReactDOM, { unmountComponentAtNode } from 'react-dom'
 import Pages from './Pages'
-import Task from '../../models/task.model'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Pages tests', () => {
 
-    const taskName = 'task1'
-    const task = new Task(1, taskName, false)
-
     it('renders without crash', () => {
         const div = document.createElement('div')
-        ReactDOM.render(<Pages task={task} reloadTasks={() => false} />, div);
+        ReactDOM.render(
+            <Pages
+                totalItems={10}
+                itemsPerPage={10}
+                currentPage={1}
+                changePage={() => false}
+            />, div);
         unmountComponentAtNode(div)
+    });
+
+    it('must toshow pagination containing 3 pages', () => {
+        const { getByTestId } = render(
+            <Pages
+                totalItems={15}
+                itemsPerPage={5}
+                currentPage={1}
+                changePage={() => false}
+            />)
+        const pagination = getByTestId('pagination')
+        expect(pagination).toHaveTextContent('1')
+        expect(pagination).toHaveTextContent('2')
+        expect(pagination).toHaveTextContent('3')
     });
 
 })
